1fa8d0ad9b647895c1a9543efde7f51c35dc9515
Ben <baka_1993@yahoo.co.uk>
2020-06-03 13:02:26 +0100

    Updates

---
+++ precache-manifest.28ba72faa268b682cabd905c61c59206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fdb4fb680dd92c61caae91e41dee20",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "264d9bfe9945b36790ae",
+    "url": "/Monsters-Rolodex/static/css/main.5b6a8f0f.chunk.css"
+  },
+  {
+    "revision": "2dcba8f4d9675eb0f4a8",
+    "url": "/Monsters-Rolodex/static/js/2.c8b6800c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monsters-Rolodex/static/js/2.c8b6800c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264d9bfe9945b36790ae",
+    "url": "/Monsters-Rolodex/static/js/main.7c858893.chunk.js"
+  },
+  {
+    "revision": "a8e2f85628f1e183c6cd",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.9967aec9.js"
+  }
+]);
